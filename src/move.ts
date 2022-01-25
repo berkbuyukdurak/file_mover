8724b65fe80d4703f33d2afada9a7fc8736d4622
C:\Users\berkb\Desktop\Workspaces\SourceTree Workspace <berkbuyukdurak07@gmail.com>
2022-01-25 19:11:09 +0300

    File and folder objects are added. List is identified as Folder[].

--- src/move.ts
+++ src/move.ts
@@ -1,5 +1,21 @@
 // Please update this type as same as with the data shape.
-type List = unknown;
+
+/**
+ * These are the objects that should be used for solving the function.
+ */
+type File = {
+  id: string,
+  name: string
+};
+
+type Folder = {
+  id: string,
+  name: string,
+  files: File[]
+}
+
+// According to the function signature, list should be Folder[] type.
+type List = Folder[];
 
 export default function move(list: List, source: string, destination: string): List {
   throw new Error('Not implemented');
